e65ab0f27a85547d66e9252183c30d7c812bddcc
Arthur Gan <arthurgan29@gmail.com>
2024-05-15 03:14:24 -0700

    Proper filepaths

--- backend.py
+++ backend.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
+import os
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import requests
 
 url = 'http://ec2-3-145-172-234.us-east-2.compute.amazonaws.com:8080'
+sep = os.sep
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
         print(f'event type: {event.event_type}  path : {event.src_path}')
-        if(event.src_path == '.\\labopen.txt'):
+        filename = event.src_path.split(sep)[-1]
+        path = event.src_path
+        if(filename == 'labopen.txt'):
             print("file has been modified")
-            new_state = open('.\\labopen.txt', 'r').read().strip()
+            new_state = open(path, 'r').read().strip()
             print("Setting room to " + new_state)
             body = {'setRoomState': int(new_state)}
             response = requests.post(url, json = body)
