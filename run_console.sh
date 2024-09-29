57722433c0e2c8ccd62b5885e9324758128489ae
Arthur Gan <arthurgan29@gmail.com>
2024-09-29 01:09:49 -0700

    colors

--- run_console.sh
+++ run_console.sh
@@ -9,6 +9,11 @@ STARTUP_CART=$HOME_DIR/carts/lab-console.p8
 BACKEND_SCREEN_NAME=p8_console_backend
 BACKEND_SCRIPT_PATH=$PWD/backend.py
 
+# terminal colors from https://en.wikipedia.org/wiki/ANSI_escape_code
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+
 echo "Running Pico-8 from: $PICO_8_PATH"
 echo "Using startup cart: $STARTUP_CART"
 echo "Using home dir: $HOME_DIR"
@@ -26,10 +31,11 @@ echo "p8 console backend started on screen: $BACKEND_SCREEN_NAME"
 # run pico-8, using our custom home directory and console startup cart
 $PICO_8_PATH -home $HOME_DIR -run $STARTUP_CART
 
-echo "=================="
-echo "Alt-Ctrl-Lab Pico8 Console exited"
-echo "type 'exit' to logout, and log back in with 'p8_console_user' to restart"
-echo "=================="
-
 # kill backend screen and process
 screen -X -S $BACKEND_SCREEN_NAME quit
+
+echo 
+echo -e "${CYAN}==================${NC}"
+echo -e "Alt-Ctrl-Lab Pico-8 console has shut down."
+echo -e "Type ${CYAN}exit${NC} to logout, then log back in with username ${CYAN}p8_console_user${NC} to restart"
+echo -e "${CYAN}==================${NC}"
