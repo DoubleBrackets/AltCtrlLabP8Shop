71fedb90f8dc980a9ab808def008e991eeb89ff1
Arthur Gan <arthurgan29@gmail.com>
2024-09-28 23:41:30 -0700

    updated script to run backend

--- run_pico_8.sh
+++ run_pico_8.sh
@@ -1,10 +1,13 @@
 # run from inside the repo directory
 
+
 PWD=$(pwd)
 PICO_8_PATH=$1
 # home dir is where the pico 8 config files are generated
 HOME_DIR=$PWD/pico8-home
 STARTUP_CART=$HOME_DIR/carts/lab-console.p8
+BACKEND_SCREEN_NAME=p8_console_backend
+BACKEND_SCRIPT_PATH=$PWD/backend.py
 
 echo "Running Pico-8 from: $PICO_8_PATH"
 echo "Using startup cart: $STARTUP_CART"
@@ -12,6 +15,12 @@ echo "Using home dir: $HOME_DIR"
 
 mkdir -p $HOME_DIR
 
-$PICO_8_PATH -home $HOME_DIR -run $STARTUP_CART
+# kill backend screen if one is running
+screen -X -S $BACKEND_SCREEN_NAME quit
 
-# /Users/arthu/Programs/Creation/PICO-8/pico8.exe -home C:/Users/arthu/AppData/Roaming/pico-8/carts/AltP8/pico8-home -run lab-console
+# start backend screen
+screen -dmS $BACKEND_SCREEN_NAME python $BACKEND_SCRIPT_PATH
+
+echo "p8 console backend started on screen: $BACKEND_SCREEN_NAME"
+
+$PICO_8_PATH -home $HOME_DIR -run $STARTUP_CART
